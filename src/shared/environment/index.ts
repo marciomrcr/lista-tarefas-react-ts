aa61886a8f59a48a65d36d9dfbf2ec9408049e11
Márcio Rodrigues <81385414+marciomrcr@users.noreply.github.com>
2022-10-06 16:07:36 -0300

    config Environment e axios

--- src/shared/environment/index.ts
+++ src/shared/environment/index.ts
@@ -1,1 +1,18 @@
-export {};
+export const Environment = {
+  /**
+   * Define a quantidade de linhas que serão carregadas por padrão nas listagem
+   */
+  LIMITE_DE_LINHAS: 10,
+  /**
+   * Placeholder exibido nos inputs
+   */
+  INPUT_DE_BUSCA: "Pesquisar...",
+  /**
+   * Texto exibido quando nenhum registro é encontrado em uma listagem
+   */
+  LISTAGEM_VAZIA: "Nenhum registro encontrado",
+  /**
+   * URL Base de consulta dessa aplicação "http://localhost:3333"
+   */
+  URL_BASE: "http://localhost:3333",
+};
